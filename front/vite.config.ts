602ee0f7f6fb61c16303a2527932915b5ce9ec78
Твое имя <hitomi.jpeg@gmail.com>
2025-06-14 20:13:04 +0300

    f

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,20 +3,12 @@ import react from "@vitejs/plugin-react";
 import UnoCSS from "unocss/vite";
 import path from "node:path";
 
-const buildTime = Date.now().toString();
-
-import { htmlVersionInject } from './vite-html-version-inject'; // создашь этот файл
-
 export default defineConfig({
-  define: {
-    __BUILD_VERSION__: JSON.stringify(buildTime),
-  },
   plugins: [
     react(),
     UnoCSS({
       configFile: path.resolve("./uno.config.ts"),
     }),
-    htmlVersionInject(buildTime),
   ],
   server: {
     host: "0.0.0.0",
