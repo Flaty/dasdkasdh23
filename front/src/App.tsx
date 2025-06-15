2c3c211027e39460e6da767cf9e5bb5315053b70
Твое имя <hitomi.jpeg@gmail.com>
2025-06-15 17:11:52 +0300

    fix

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -33,6 +33,11 @@ interface ExtendedWebApp {
   disableClosingConfirmation(): void;
   enableVerticalSwipes(): void;
   disableVerticalSwipes(): void;
+  HapticFeedback: {
+    impactOccurred(style: 'light' | 'medium' | 'heavy' | 'rigid' | 'soft'): void;
+    notificationOccurred(type: 'error' | 'success' | 'warning'): void;
+    selectionChanged(): void;
+  };
 }
 
 declare global {
